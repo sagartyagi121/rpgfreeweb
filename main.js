525e86d12037b91f2df6d920c32f08fd1d8c6c93
CodeCommando <sagartyagi.tyagi@gmail.com>
2020-01-14 14:38:31 +0530

    modified package

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  win.loadFile('index.html')
+  win.loadFile('/public/index.html')
 
   // Open the DevTools.
   win.webContents.openDevTools()
